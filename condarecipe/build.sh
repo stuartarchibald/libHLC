af6295daa8ba26512b728a54ceba3d228475c84a
Stuart Archibald <stuart.archibald@googlemail.com>
2016-06-01 12:58:05 +0100

    Fix conda build
    WIP on fixing up conda. License text inclusion needs some work
    rest of package seems to build and test ok.

--- condarecipe/build.sh
+++ condarecipe/build.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DLLVM_DIR=${LLVM_MAINLINE}/lib/cmake/llvm/
 make
-mkdir $PREFIX/lib
-cp libHLC.so $PREFIX/lib
-cp builtins-hsail.opt.bc $PREFIX/lib
+mkdir ${PREFIX}/lib
+cp "libhlc/libHLC.so" "${PREFIX}/lib"
+cp "libhlc/test/builtins-hsail.opt.bc" "${PREFIX}/lib"
+cp "libhlc/test/hsail-amdgpu-wrapper.ll" "${PREFIX}/lib"
+ctest -V
